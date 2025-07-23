ffc00ae75720625f889dfd1877cc0001f8447810
Samuel Greeley <sgreeley@gmail.com>
2025-07-23 00:48:52 -0400

    updates

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -16,10 +16,15 @@ if status is-interactive
     set -Ux CARAPACE_BRIDGES 'zsh,fish,bash' # optional
     carapace _carapace | source
 
+    #atuin
+    atuin init fish | source
+
+    #aliases
     alias cz="chezmoi"
     alias cg="cd /home/sgreeley/.local/share/chezmoi"
     alias top="btm"
     alias f='fzf --preview '\''bat --color=always --style=numbers --line-range=:500 {}'\'
     alias fe='fd --type f --hidden --exclude .git | fzf | xargs nvim'
     alias df="dysk"
+    alias ls="eza"
 end
